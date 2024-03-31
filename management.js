e574e726380a902daa1029b10b29661edccdd9d2
yherrera0044 <123136290+yherrera0044@users.noreply.github.com>
2024-03-30 22:42:22 -0700

    Add files via upload

---
+++ management.js
@@ -0,0 +1,12 @@
+function britanica() {
+  window.open('https://www.britannica.com/science/mental-hygiene');
+}
+function meditate() {
+  window.open('https://www.mindful.org/how-to-meditate/');
+}
+function explore() {
+  window.open('https://mindstories.org/');
+}
+function start_today() {
+  window.open('https://forms.gle/fosLTaw7SaiQeQc37');
+}
