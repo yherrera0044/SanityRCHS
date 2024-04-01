498c32d8cced80f4de4b3a9909dbfdd810543cb8
Yonatan <yherrera0044@mymail.lausd.net>
2024-04-01 14:52:06 -0700

    working on it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-  content: ['./{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = ['./{html,js}'];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
